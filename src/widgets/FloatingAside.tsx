f0901ad408a7a4ffba089717f302833e4b451fa7
minkyu_Park <kyle970320@farmzonesnc.com>
2024-11-11 14:16:37 +0900

    add sticky position in header and sidebar

--- src/widgets/FloatingAside.tsx
+++ src/widgets/FloatingAside.tsx
@@ -3,7 +3,8 @@ import styles from "@/styles/flotingAside.module.scss";
 export default function FloatingAside() {
   return (
     <aside className={styles.aside}>
-      <span>123123</span>
+      <div>123</div>
+      <div>123</div>
     </aside>
   );
 }
