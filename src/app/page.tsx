7ed6b7cc45bdc354bfca79be8e07b628da5ef9fe
minkyu_Park <kyle970320@farmzonesnc.com>
2024-12-12 15:43:56 +0900

    add favorite list in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import MainList from "@/widgets/MainList";
 import FloatingAside from "@/widgets/FloatingAside";
 import { Aldrich } from "next/font/google";
 import ListTitle from "@/components/ListTitle";
+import FavoriteList from "@/components/FavoriteList";
 import styles from "./page.module.css";
 import Slider from "../components/Slider";
 import TypingModal from "../components/TypingModal";
@@ -11,6 +12,7 @@ const headerFont = Aldrich({ subsets: ["latin"], weight: ["400"] });
 
 export default function Home() {
   const message = "Make the code concise";
+
   return (
     <div className={styles.page}>
       <div className={styles.pageTitle}>
@@ -20,7 +22,7 @@ export default function Home() {
       </div>
       <div>
         <ListTitle text="Favorites" />
-        <div>Save your favorite articles!</div>
+        <FavoriteList />
       </div>
       <main className={styles.main}>
         <FloatingAside />
