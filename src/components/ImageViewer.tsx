56a26fd5ead69b33631c70be3233555895abb7af
minkyu_Park <kyle970320@farmzonesnc.com>
2025-02-11 18:23:02 +0900

    add image drag func

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -12,100 +12,181 @@ import {
   CgCornerUpRight,
 } from "react-icons/cg";
 import { LuZoomIn, LuZoomOut } from "react-icons/lu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { produce } from "immer";
+import useBodyScrollLock from "@/hooks/useBodyScrollLock";
+import { getZoomBlockStyle } from "@/utils/imageControll";
 
 interface Props {
   url: string;
   closeViewer: () => void;
 }
 
+type Axis = "X" | "Y" | "Z";
 export default function ImageViewer({ url, closeViewer }: Props) {
-  const handleCloseViewer = () => {
-    closeViewer();
-  };
-
-  const [imageRotate, setImageRotate] = useState<string>("rotate(0deg)");
+  const { openScroll, lockScroll } = useBodyScrollLock();
+  const [zoomBlock, setZoomBlock] = useState<{
+    zoomIn: boolean;
+    zoomOut: boolean;
+  }>({ zoomIn: false, zoomOut: false });
+  const [isMouseHold, setIsMouseHold] = useState<boolean>(false);
   const [rotateLeftAndRight, setRotateLeftAndRight] = useState<number>(0);
-  // const setOriginRotateZ = (rotateNum: number) => {
-  //   const rorateNumSign = rotateNum > 0 ? 1 : -1;
-  //   for (let i = 0; i <= Math.abs(rotateNum) / 4; i++) {
-  //     if ((i + 1) * 4) {
-  //       return;
-  //     }
-  //   }
-  // };
-  const handleControlImage = (imageRotateValue: string) => {
-    if (
-      imageRotateValue.startsWith("rotateX") ||
-      imageRotateValue.startsWith("rotateY")
-    ) {
-      setRotateLeftAndRight(0);
-      if (imageRotate === imageRotateValue) {
-        setImageRotate("rotate(0deg)");
-        return;
-      }
-      if (imageRotate === "rotate(0deg)") {
-        setImageRotate(imageRotateValue);
-        return;
-      }
-      setImageRotate("");
-      setTimeout(() => {
-        setImageRotate(imageRotateValue);
-      }, 500);
-    }
+  const [imageStartPoint, setImageStartPoint] = useState<Record<Axis, number>>({
+    X: 1,
+    Y: 1,
+    Z: 1,
+  });
+  const [imageScale, setImageScale] = useState<Record<Axis, number>>({
+    X: 1,
+    Y: 1,
+    Z: 1,
+  });
+  const [imageTranslate, setImageTranslate] = useState<Record<Axis, number>>({
+    X: 0,
+    Y: 0,
+    Z: 0,
+  });
 
-    if (imageRotateValue.startsWith("rotateZ")) {
-      if (
-        imageRotate.startsWith("rotateX") ||
-        imageRotate.startsWith("rotateY")
-      ) {
-        setImageRotate("");
-        setTimeout(() => {
-          setImageRotate(imageRotateValue);
-        }, 500);
-        return;
-      }
-      setImageRotate(imageRotateValue);
+  const handleImageScale = (
+    type: "flip" | "scale",
+    rotateKey: Axis,
+    rotateValue: number,
+  ) => {
+    if (type === "flip") {
+      setImageScale(
+        produce((draft) => {
+          draft[rotateKey] = imageScale[rotateKey] * -1;
+        }),
+      );
+      return;
     }
+    setImageScale(
+      produce((draft) => {
+        draft[rotateKey] = rotateValue;
+      }),
+    );
   };
-  console.log(rotateLeftAndRight);
+  const handleImageStartPoint = (startKey: Axis, startValue: number) => {
+    setImageStartPoint(
+      produce((draft) => {
+        draft[startKey] = startValue;
+      }),
+    );
+  };
+  const handleImageTranslate = (translateKey: Axis, translateValue: number) => {
+    setImageTranslate(
+      produce((draft) => {
+        draft[translateKey] = translateValue;
+      }),
+    );
+  };
+
+  useEffect(() => {
+    lockScroll();
+
+    return () => {
+      openScroll();
+    };
+  }, [lockScroll, openScroll]);
+
+  useEffect(() => {
+    if (isMouseHold) {
+      const handlePointerMove = (e: PointerEvent) => {
+        requestAnimationFrame(() => {
+          const deltaX = e.clientX - imageStartPoint.X;
+          const deltaY = e.clientY - imageStartPoint.Y;
+
+          // deltaX = Math.max(-MAX_SPEED, Math.min(MAX_SPEED, deltaX));
+          // deltaY = Math.max(-MAX_SPEED, Math.min(MAX_SPEED, deltaY));
+
+          handleImageTranslate("X", imageTranslate.X + deltaX);
+          handleImageTranslate("Y", imageTranslate.Y + deltaY);
+
+          handleImageStartPoint("X", e.clientX);
+          handleImageStartPoint("Y", e.clientY);
+        });
+      };
+
+      const handlePointerUp = () => {
+        setIsMouseHold(false);
+        document.removeEventListener("pointermove", handlePointerMove);
+        document.removeEventListener("pointerup", handlePointerUp);
+      };
+
+      document.addEventListener("pointermove", handlePointerMove);
+      document.addEventListener("pointerup", handlePointerUp);
+
+      return () => {
+        document.removeEventListener("pointermove", handlePointerMove);
+        document.removeEventListener("pointerup", handlePointerUp);
+      };
+    }
+  }, [isMouseHold, imageStartPoint, imageTranslate]);
+
   return createPortal(
-    <div className="imageViewerWrapper" onClick={handleCloseViewer}>
+    <div
+      className="imageViewerWrapper"
+      onMouseDown={closeViewer}
+      style={{ cursor: isMouseHold ? "grabbing" : "default" }}
+    >
       <div className="viewerPort">
         <div
-          onClick={(e) => {
+          onMouseDown={(e) => {
             e.stopPropagation();
           }}
           className="viewerContent"
-          style={{ transform: imageRotate }}
+          style={{
+            transform: `rotateZ(${rotateLeftAndRight * 90}deg) scale3d(${imageScale.X},${imageScale.Y},${imageScale.Z}) translate3d(${imageTranslate.X}px,${imageTranslate.Y}px,${imageTranslate.Z}px)`,
+          }}
         >
-          <Image src={url} alt="" fill style={{ objectFit: "contain" }} />
+          <Image
+            src={url}
+            alt=""
+            draggable={false}
+            fill
+            style={{
+              objectFit: "contain",
+              cursor: isMouseHold ? "grabbing" : "grab",
+            }}
+            onMouseDown={(e) => {
+              handleImageStartPoint("X", e.clientX);
+              handleImageStartPoint("Y", e.clientY);
+              setIsMouseHold(true);
+            }}
+            onMouseUp={() => {
+              setIsMouseHold(false);
+            }}
+            onMouseMove={() => {
+              if (!isMouseHold) {
+                return;
+              }
+            }}
+          />
         </div>
       </div>
       <div
         className="viewerButtons"
         key={url}
-        onClick={(e) => {
+        onMouseDown={(e) => {
           e.stopPropagation();
         }}
       >
         <CgArrowsVAlt
           className="icon"
           onClick={() => {
-            handleControlImage("rotateX(180deg)");
+            handleImageScale("flip", "Y", 180);
           }}
         />
         <CgArrowsHAlt
           className="icon"
           onClick={() => {
-            handleControlImage("rotateY(180deg)");
+            handleImageScale("flip", "X", 180);
           }}
         />
         <CgCornerUpLeft
           onClick={() => {
             const newRotateCount = rotateLeftAndRight - 1;
             setRotateLeftAndRight(newRotateCount);
-            handleControlImage(`rotateZ(${newRotateCount * 90}deg)`);
           }}
           className="icon"
         />
@@ -113,12 +194,49 @@ export default function ImageViewer({ url, closeViewer }: Props) {
           onClick={() => {
             const newRotateCount = rotateLeftAndRight + 1;
             setRotateLeftAndRight(newRotateCount);
-            handleControlImage(`rotateZ(${newRotateCount * 90}deg)`);
           }}
           className="icon"
         />
-        <LuZoomIn className="icon" />
-        <LuZoomOut className="icon" />
+        <LuZoomIn
+          className="icon"
+          onClick={() => {
+            if (zoomBlock.zoomIn) {
+              return;
+            }
+            const newZoomX = imageScale.X * 1.5;
+            const newZoomY = imageScale.Y * 1.5;
+            if (newZoomX > 2) {
+              setZoomBlock(
+                produce((draft) => {
+                  draft.zoomIn = true;
+                }),
+              );
+            }
+            handleImageScale("scale", "X", newZoomX);
+            handleImageScale("scale", "Y", newZoomY);
+          }}
+          style={{ ...getZoomBlockStyle(zoomBlock.zoomIn) }}
+        />
+        <LuZoomOut
+          className="icon"
+          onClick={() => {
+            if (zoomBlock.zoomOut) {
+              return;
+            }
+            const newZoomX = imageScale.X / 1.5;
+            const newZoomY = imageScale.Y / 1.5;
+            if (newZoomX < 0.5) {
+              setZoomBlock(
+                produce((draft) => {
+                  draft.zoomOut = true;
+                }),
+              );
+            }
+            handleImageScale("scale", "X", newZoomX);
+            handleImageScale("scale", "Y", newZoomY);
+          }}
+          style={{ ...getZoomBlockStyle(zoomBlock.zoomOut) }}
+        />
       </div>
     </div>,
     document.getElementById("image-viewer-root") as HTMLElement,
